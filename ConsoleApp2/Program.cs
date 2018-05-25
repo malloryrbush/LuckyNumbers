7db12b2b89e057f386557cb0b71bb4600ab1519f
malloryrbush <malloryrbush@hotmail.com>
2018-05-25 12:33:47 -0400

    added initial console writelines

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,26 @@ namespace LuckyNumbers
     {
         static void Main(string[] args)
         {
+            //DECLARING VARIABLES
+            int startNum;
+            int endNum;
+            int guess;
+            //END DECLARING VARIABLES 
+
+            //WRITELINE LOWEST RANGE NUMBER INPUT
+            Console.WriteLine("Please choose the lowest number you would like in your guessing range.");
+            startNum = int.Parse(Console.ReadLine());
+            //END WRITELINE LOWEST RANGE NUMBER INPUT
+
+            //WRITELINE HIGHEST RANGE NUMBER INPUT
+            Console.WriteLine("Please choose the highest number you would like in your guessing range.");
+            endNum = int.Parse(Console.ReadLine());
+            //END WRITELINE HIGHEST RANGE NUMBER INPUT
+
+            //WRITELINE ASK FOR 6 NUMBERS
+            Console.WriteLine("Enter any number between " + (startNum) + " and " + (endNum) + " !");
+            guess = int.Parse(Console.ReadLine());
+            //END WRITELINE ASK FOR 6 NUMBERS
         }
     }
 }
