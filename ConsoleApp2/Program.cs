989fb9dec2da6da21fa5c4ef58c47bd879698254
malloryrbush <malloryrbush@hotmail.com>
2018-05-25 16:13:49 -0400

    fixed while loop for user input 6 numbers

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -32,23 +32,32 @@ namespace LuckyNumbers
             {
                 Console.WriteLine("Enter any number between " + (startNum) + " and " + (endNum) + " !");
                 userGuess = int.Parse(Console.ReadLine());
-                guessNums[0] = (userGuess);//ARRAY
-               
+
+                while ((userGuess < startNum || userGuess > endNum))//VALIDATE THAT NUMBER INPUT IS WITHIN RANGE
+                {
+                    Console.WriteLine("Oops! The number you entered is outside of the range you chose. Please try again.");
+                    userGuess = (int.Parse(Console.ReadLine()));//END VALIDATE THAT NUMBER INPUT IS WITHIN RANGE
+                }
+                guessNums[i] = userGuess;//ARRAY
             }
-            //END USER INPUT 6 GUESSES FOR LOOP
 
-            //RANDOM NUMBER GENERATOR
-            int[] RandNum = new int[6];
-            Random r = new Random();
-            for (int j = 0; j < RandNum.Length; i++)
-            {
-                RandNum[j] = r.Next(startNum, endNum + 1);
+                //END USER INPUT 6 GUESSES FOR LOOP
+
+                //RANDOM NUMBER GENERATOR
+                int[] RandNum = new int[6];
+                    Random r = new Random();
+                    for (int j = 0; j < RandNum.Length; j++)
+                    {
+                        RandNum[j] = r.Next(startNum, endNum);
+                    }
+                    //END RANDOM NUMBER GENERATOR
+
+                    //VALIDATE THAT INPUT NUMBERS ARE WITHIN RANGE
+
+
+                } //CLOSE MAIN METHOD
             }
-            //END RANDOM NUMBER GENERATOR
+        }
+    
 
 
-
-
-        } //CLOSE MAIN METHOD
-    }
-}
