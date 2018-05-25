65ef521f30169a74941d82f507c59a98d688c63c
malloryrbush <malloryrbush@hotmail.com>
2018-05-25 14:36:24 -0400

    added user input numbers loop and store as array

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -13,23 +13,42 @@ namespace LuckyNumbers
             //DECLARING VARIABLES
             int startNum;
             int endNum;
-            int guess;
+            int userGuess;
             //END DECLARING VARIABLES 
 
             //WRITELINE LOWEST RANGE NUMBER INPUT
-            Console.WriteLine("Please choose the lowest number you would like in your guessing range.");
+            Console.WriteLine("Please choose the lowest number you would like in your lottery.");
             startNum = int.Parse(Console.ReadLine());
             //END WRITELINE LOWEST RANGE NUMBER INPUT
 
             //WRITELINE HIGHEST RANGE NUMBER INPUT
-            Console.WriteLine("Please choose the highest number you would like in your guessing range.");
+            Console.WriteLine("Please choose the highest number you would like in your lottery.");
             endNum = int.Parse(Console.ReadLine());
             //END WRITELINE HIGHEST RANGE NUMBER INPUT
 
-            //WRITELINE ASK FOR 6 NUMBERS
-            Console.WriteLine("Enter any number between " + (startNum) + " and " + (endNum) + " !");
-            guess = int.Parse(Console.ReadLine());
-            //END WRITELINE ASK FOR 6 NUMBERS
-        }
+            //USER INPUT 6 GUESSES FOR LOOP
+            int[] guessNums = new int[6];//ARRAY
+            for (int i = 0; i < guessNums.Length; i++)
+            {
+                Console.WriteLine("Enter any number between " + (startNum) + " and " + (endNum) + " !");
+                userGuess = int.Parse(Console.ReadLine());
+                guessNums[0] = (userGuess);//ARRAY
+               
+            }
+            //END USER INPUT 6 GUESSES FOR LOOP
+
+            //RANDOM NUMBER GENERATOR
+            int[] RandNum = new int[6];
+            Random r = new Random();
+            for (int j = 0; j < RandNum.Length; i++)
+            {
+                RandNum[j] = r.Next(startNum, endNum + 1);
+            }
+            //END RANDOM NUMBER GENERATOR
+
+
+
+
+        } //CLOSE MAIN METHOD
     }
 }
